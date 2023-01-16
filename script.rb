e4ce7198bab683aa43df673d6a361565ef331c14
MclPio <113801014+MclPio@users.noreply.github.com>
2023-01-16 16:30:20 -0500

    create bubble sort function

--- script.rb
+++ script.rb
@@ -5,10 +5,24 @@
 
 =end
 
-array = [4,3,78,2,0,2]
+data = [4,3,78,2,0,2]
 
-def bubble_sort(array)
-
+def bubble_sort(data)
+  array = data
+  count = 0
+  while count < data.length
+    array.each_with_index do |item, idx|
+      if idx < array.length-1
+        if array[idx] > array[idx+1]
+          before_idx = array[idx]
+          array[idx] = array[idx+1]
+          array[idx+1] = before_idx
+        end
+      end
+    end
+    count += 1
+  end
+  array
 end
 
-p bubble_sort(array)
+p bubble_sort(data)
